bd203586f315b352fbbaee5f6945f17c8b005fcd
licht <362829410@qq.com>
2018-05-26 23:53:36 +0800

    tn

--- android/app/src/main/java/com/stview/MainActivity.java
+++ android/app/src/main/java/com/stview/MainActivity.java
@@ -6,7 +6,7 @@ public class MainActivity extends ReactActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * This is used to schedule rendering of the component.citxiu
      */
     @Override
     protected String getMainComponentName() {
